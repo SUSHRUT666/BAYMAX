27f26158cdc50405e71a801fe957999f9a046b0d
SUSHRUT666 <155754411+SUSHRUT666@users.noreply.github.com>
2025-03-30 20:02:24 +0530

    Update Features.jsx

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -13,17 +13,17 @@ const Features = () => {
             <div className="bg-amber-50 shadow-lg rounded-lg p-6 text-center hover:scale-110  duration-300 border border-black">
             <img src="https://static.vecteezy.com/system/resources/previews/013/218/336/non_2x/herbal-capsule-pill-leaf-medicine-logo-icon-illustration-template-capsule-pharmacy-medical-logo-template-vector.jpg" alt="23 coffees to choose" className="w-16 h-16 mx-auto mb-4" />
             <h3 className="text-xl font-semibold mb-2 font-montserrat">Medicine Advice</h3>
-            <p className="text-gray-600 font-montserrat">Morbi porttitor dignissim leo, eget bibendum diam.</p>
+            <p className="text-gray-600 font-montserrat">Your trusted guide for personalized medicine advice—empowering healthier choices, one click at a time!</p>
             </div>
             <div className="bg-amber-50 shadow-lg rounded-lg p-6 text-center hover:scale-110 duration-300 border border-black">
             <img src="https://cdn3.f-cdn.com//files/download/204824703/Doctor%20Consult.jpg?width=780&height=780&fit=crop" alt="Pleasant ambient" className="w-16 h-16 mx-auto mb-4" />
             <h3 className="text-xl font-semibold mb-2 font-montserrat">Doctor appointment</h3>
-            <p className="text-gray-600 font-montserrat">Aliquam erat volutpat. Nam egestas, diam vitae vehicula vehicula.</p>
+            <p className="text-gray-600 font-montserrat">"Connecting you to expert care—schedule your doctor’s appointment effortlessly, anytime, anywhere!</p>
             </div>
             <div className="bg-amber-50 shadow-lg rounded-lg p-6 text-center hover:scale-110 duration-300 border border-black">
             <img src="https://i.pinimg.com/originals/30/7e/69/307e6906c251d91bb6202b3dd4736d7a.jpg" alt="Pleasant ambient" className="w-16 h-16 mx-auto mb-4" />
             <h3 className="text-xl font-semibold mb-2 font-montserrat">keep track</h3>
-            <p className="text-gray-600 font-montserrat">Cras convallis purus vestibulum turpis tempus euismod.</p>
+            <p className="text-gray-600 font-montserrat">"Stay organized and in control—track your health records and appointments with ease, all in one place!.</p>
             </div>
         </div>
 
